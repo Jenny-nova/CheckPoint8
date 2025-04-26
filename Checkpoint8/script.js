0ff8490be8309f0f4cb8c354a5210566a7477773
Jenny <jeniferfernandezares@gmail.com>
2025-04-26 19:23:33 +0200

    Add files via upload

---
+++ Checkpoint8/script.js
@@ -0,0 +1,17 @@
+// Ejercicio 1
+const miLista = ["velma", "exploradora", "jane", "john", "harry"];
+for (let i=0; i<miLista.length; i++ ) {
+    console.log(miLista[i]);
+}
+
+// Ejercicio 2
+const miLista = ["velma", "exploradora", "jane", "john", "harry"];
+let i=0;
+while (i < miLista.length) {
+    console.log(miLista[i]);
+    i++;
+}
+
+// Ejercicio 3
+const saludar = () => "Hola mundo";
+console.log(saludar());
